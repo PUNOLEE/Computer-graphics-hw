2f299571ba909f1b942e2222494806dc78d09d5a
PUNOLEE <punolee@outlook.com>
2019-01-26 17:35:05 -0600

    second commit

--- projectA/basicJS/lib1.js
+++ projectA/basicJS/lib1.js
@@ -1,4 +1,16 @@
-//Fragment shader program
+var VSHADER_SOURCE =
+  'attribute vec4 a_Position;\n' +
+  'attribute vec4 a_Color;\n' +
+  'attribute float a_PointSize;\n' +
+  'uniform mat4 u_ModelMatrix;\n' +
+  'varying vec4 v_Color;\n' +
+  'void main() {\n' +
+  '  gl_Position = u_ModelMatrix * a_Position;\n' +
+  '  v_Color = a_Color;\n' +
+  '  gl_PointSize = a_PointSize;\n' +
+  '}\n';
+
+// Fragment shader program
 var FSHADER_SOURCE =
   '#ifdef GL_ES\n' +
   'precision mediump float;\n' +
@@ -6,38 +18,6 @@ var FSHADER_SOURCE =
   'varying vec4 v_Color;\n' +
   'void main() {\n' +
   '  gl_FragColor = v_Color;\n' +
-  '}\n';
-
-//Vertex shader program
-var VSHADER_SOURCE =
-  'attribute vec4 a_Position;\n' +
-  'attribute vec4 a_Color;\n' +
-  'attribute float a_PointSize;\n' +
-  'attribute vec4 a_Normal;\n' +
-  'uniform mat4 u_MvpMatrix;\n' +
-  'uniform mat4 u_ModelMatrix;\n' +    // Model matrix
-  'uniform mat4 u_NormalMatrix;\n' +   // Coordinate transformation matrix of the normal
-  'uniform vec3 u_LightColor;\n' +     // Light color
-  'uniform vec3 u_LightPosition;\n' +  // Position of the light source
-  'uniform vec3 u_AmbientLight;\n' +   // Ambient light color
-  'varying vec4 v_Color;\n' +
-  'void main() {\n' +
-  '  gl_Position = u_MvpMatrix * a_Position;\n' +
-     // Recalculate the normal based on the model matrix and make its length 1.
-  '  vec3 normal = normalize(vec3(u_NormalMatrix * a_Normal));\n' +
-     // Calculate world coordinate of vertex
-  '  vec4 vertexPosition = u_ModelMatrix * a_Position;\n' +
-     // Calculate the light direction and make it 1.0 in length
-  '  vec3 lightDirection = normalize(u_LightPosition - vec3(vertexPosition));\n' +
-     // Calculate the dot product of the normal and light direction
-  '  float nDotL = max(dot(normal, lightDirection), 0.0);\n' +
-     // Calculate the color due to diffuse reflection
-  '  vec3 diffuse = u_LightColor * a_Color.rgb * nDotL;\n' +
-     // Calculate the color due to ambient reflection
-  '  vec3 ambient = u_AmbientLight * a_Color.rgb;\n' +
-     // Add the surface colors due to diffuse reflection and ambient reflection
-  '  v_Color = vec4(diffuse + ambient, a_Color.a);\n' + 
-  '  gl_PointSize = a_PointSize;\n' +
   '}\n';
 
 //global variables
--- projectA/basicJS/projectA.js
+++ projectA/basicJS/projectA.js
@@ -1,16 +1,17 @@
 var ANGLE_STEP = 30.0;
 
-var g_mvpMatrix = new Matrix4();
-var g_normalMatrix = new Matrix4(); // Coordinate transformation matrix for normals
-
 var g_last = Date.now(); // Timestamp for most-recently-drawn image;
 var isDrag = false;
 var xMouseclik = 0.0;
 var yMouseclik = 0.0;
 var step = 0.1;
-var x = 0;
-var y = 0;
+var myX = 0.0;
+var myY = 0.0;
+var heartX = 0.0;
+var heartY = 0.0;
 var currentAngle = 0.0;
+var cAngle2 = 0.0;
+var cAngle2Step = 100.0
 var right = true;
 
 function main() {
@@ -24,44 +25,13 @@ function main() {
   gl.clearColor(0.25, 0.25, 0.25, 1); // set new screen-clear color, RGBA
   // (for WebGL framebuffer, not canvas)
 
-  // Set the vertex coordinates and color
-  var n = initVertexBuffers(gl);
-  if (n < 0) {
-    console.log("Failed to set the vertex information");
-    return;
-  }
-
-  // Get the storage location of u_MvpMatrix,u_NormalMatrix,u_LightColor,u_LightPosition,u_AmbientLight
-  var u_MvpMatrix = gl.getUniformLocation(gl.program, "u_MvpMatrix");
-  var u_NormalMatrix = gl.getUniformLocation(gl.program, "u_NormalMatrix");
-  var u_LightColor = gl.getUniformLocation(gl.program, "u_LightColor");
-  var u_LightPosition = gl.getUniformLocation(gl.program, "u_LightPosition");
-  var u_AmbientLight = gl.getUniformLocation(gl.program, "u_AmbientLight");
-  if (
-    !u_MvpMatrix ||
-    !u_NormalMatrix ||
-    !u_LightColor ||
-    !u_LightPosition ||
-    !u_AmbientLight
-  ) {
-    console.log("Failed to get the storage location");
-    return;
-  }
-
-  var vpMatrix = new Matrix4(); // View projection matrix
-  vpMatrix.setPerspective(30, canvas.width / canvas.height, 1, 100);
-  vpMatrix.lookAt(6, 6, 14, 0, 0, 0, 0, 1, 0);
-
-  // Set the light color (white)
-  gl.uniform3f(u_LightColor, 1.0, 1.0, 1.0);
-  // Set the light direction (in the world coordinate)
-  gl.uniform3f(u_LightPosition, 2.3, 4.0, 3.5);
-  // Set the ambient light
-  gl.uniform3f(u_AmbientLight, 0.2, 0.2, 0.2);
-
+   
+   makeMess();
+   makeHeart();
+   
   // onkeydown listener
   document.onkeydown = function(ev) {
-    keydown(ev, gl, n, vpMatrix, u_MvpMatrix, u_NormalMatrix);
+    keydown(ev, gl);
   };
   // onmousedown listener
   canvas.onmousedown = function(ev) {
@@ -78,9 +48,9 @@ function main() {
 
   //tick function -> animation
   var tick = function() {
-    currentAngle = animate(currentAngle); // Update the rotation angle
+    animate(); // Update the rotation angle
 
-    draw(gl, n, vpMatrix, u_MvpMatrix, u_NormalMatrix); // draw objects
+    draw(gl); // draw objects
 
     requestAnimationFrame(tick, canvas); // Request that the browser ?calls tick
   };
@@ -92,7 +62,16 @@ function submitA() {
   var userInput = document.getElementById("userInput").value;
   //set step using user input
   ANGLE_STEP = userInput;
+  console.log("your mess moving speed now is " + ANGLE_STEP);
   document.getElementById("userInput").value = "";
+}
+
+function submitB() {
+  var userInputHeart = document.getElementById("userInputHeart").value;
+  //set step using user input
+  cAngle2Step = userInputHeart;
+  console.log("your heart moving speed now is " + cAngle2Step);
+  document.getElementById("userInputHeart").value = "";
 }
 
 function myMouseDown(ev, gl, canvas) {// Called when user PRESSES down any mouse button;
@@ -110,6 +89,8 @@ function myMouseDown(ev, gl, canvas) {// Called when user PRESSES down any mouse
     (canvas.height / 2);
 
   isDrag = true; // set our mouse-dragging flag
+  if(isDrag)
+    console.log("you are dragging your mess");
   xMouseclik = cx; // record where mouse-dragging began
   yMouseclik = cy;
 }
@@ -132,8 +113,8 @@ function myMouseMove(ev, gl, canvas) {// Called when user MOVES the mouse with a
     (canvas.height / 2);
 
   // find how far we dragged the mouse:
-  x += cx - xMouseclik; // Accumulate change-in-mouse-position,&
-  y += cy - yMouseclik;
+  myX += cx - xMouseclik; // Accumulate change-in-mouse-position,&
+  myY += cy - yMouseclik;
   xMouseclik = cx; // Make next drag-measurement from here.
   yMouseclik = cy;
 }
@@ -151,24 +132,25 @@ function myMouseUp(ev, gl, canvas) {// Called when user LEAVES any mouse button;
     (yp - canvas.height / 2) / //                     -1 <= y < +1.
     (canvas.height / 2);
   isDrag = false;
-  x += cx - xMouseclik;
-  y += cy - yMouseclik;
+  myX += cx - xMouseclik;
+  myY += cy - yMouseclik;
 
 }
 
-function keydown(ev, gl, n, vpMatrix, u_MvpMatrix, u_NormalMatrix) {// Called when user hits any key button;
+function keydown(ev, gl) {// Called when user hits any key button;
+  console.log("You are hitting keyboard ")
   switch (ev.keyCode) {
     case 65: //a
-      x -= step;
+      heartX -= step;
       break;
     case 68: //d
-      x += step;
+      heartY += step;
       break;
     case 40: // Up arrow key
-      y -= step;
+      myY -= step;
       break;
     case 38: // Down arrow key
-      y += step;
+      myY += step;
       break;
     case 39: // Right arrow key -> the positive rotation around the y-axis
       currentAngle = (currentAngle + 3.0) % 360;
@@ -179,158 +161,156 @@ function keydown(ev, gl, n, vpMatrix, u_MvpMatrix, u_NormalMatrix) {// Called wh
     default:
       return; // Skip drawing at no effective action
   }
-  // Draw dodecahedrons
-  draw(gl, n, vpMatrix, u_MvpMatrix, u_NormalMatrix);
+  // Draw mess
+  draw(gl);
 }
 
-function draw(gl, n, vpMatrix, u_MvpMatrix, u_NormalMatrix) {
-  //draw single dodecahedron
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+function draw(gl) {
+  //draw images
+  gl.clear(gl.COLOR_BUFFER_BIT);
 
-  var base = 0.5;
-  modelMatrix.setRotate(currentAngle, 0, 1, 0); // Rotate around the y-axis
-  drawDod(gl, n, base, vpMatrix, u_MvpMatrix, u_NormalMatrix);
+  var base = 0.2;
+  modelMatrix.setTranslate(0.0, 0.0, 0.0); 
 
-  var next = 1.0;
-  modelMatrix.setRotate(currentAngle, 0, 0, 1); // Rotate around the  z-axis
-  modelMatrix.translate(1.26, 2, 0.5);
-  drawDod(gl, n, next, vpMatrix, u_MvpMatrix, u_NormalMatrix);
+  modelMatrix.rotate(currentAngle, 0, 1, 0); // Rotate around the y-axis
+  
+  drawMess(gl, base);
 
-  var third = 1.5;
-  modelMatrix.setRotate(currentAngle, 1, 0, 1); // Rotate around the x & z-axis
-  modelMatrix.translate(-1.5, 0.3, 2.53);
-  drawDod(gl, n, third, vpMatrix, u_MvpMatrix, u_NormalMatrix);
+
+  var two = 0.3;
+
+  modelMatrix.translate(0.0, 0.31, 0.34); 
+  modelMatrix.translate(0.5, 0.0, 0.0);
+
+  drawMess(gl, two);
+
+  
 }
 
-//draw single dodecahedron
-function drawDod(gl, n, width, vpMatrix, u_MvpMatrix, u_NormalMatrix) {
+//draw single mess
+function drawMess(gl, size) {
   
   pushMatrix(modelMatrix);
-  modelMatrix.scale(width, width, width);
 
-  modelMatrix.translate(x, y, 0.0);
-
+  modelMatrix.scale(size, size, size);
+  modelMatrix.translate(myX, myY, 0.0);
   // Calculate the model matrix
-  //modelMatrix.translate(x,y,0.0);
   // Pass the model matrix to u_ModelMatrix
   updateModelMatrix(modelMatrix);
 
-  // Pass the model view projection matrix to u_MvpMatrix
-  g_mvpMatrix.set(vpMatrix).multiply(modelMatrix);
-  gl.uniformMatrix4fv(u_MvpMatrix, false, g_mvpMatrix.elements);
+  // Draw the mess
+  gl.drawArrays(gl.LINE_LOOP,0,20);
+  
+  modelMatrix = popMatrix(); // Retrieve the model matrix 
 
-  // Pass the matrix to transform the normal based on the model matrix to u_NormalMatrix
-  g_normalMatrix.setInverseOf(modelMatrix);
-  g_normalMatrix.transpose();
-  gl.uniformMatrix4fv(u_NormalMatrix, false, g_normalMatrix.elements);
+  // Draw heart
+  drawHeart(gl,size);
 
-  // Draw the dodecahedron
-  gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
-  modelMatrix = popMatrix(); // Retrieve the model matrix
-  //console.log("draw")
 }
 
-function animate(angle) {
+function drawHeart(gl, size){
+
+  pushMatrix(modelMatrix);
+  modelMatrix.rotate(cAngle2, 1, 1, 0); // Rotate around the x & y-axis
+
+  modelMatrix.scale(size,size,size); 
+  modelMatrix.translate(heartX, heartY, 0.0);
+
+  updateModelMatrix(modelMatrix);
+  gl.drawArrays(gl.LINE_STRIP,20,350);
+  modelMatrix = popMatrix(); // Retrieve the model matrix
+}
+
+
+function animate() {
   // Calculate the elapsed time
   var now = Date.now();
   var elapsed = now - g_last;
   g_last = now;
   // Update the current rotation angle (adjusted by the elapsed time)
-  var newAngle = angle + (ANGLE_STEP * elapsed) / 1000.0;
-  return (newAngle %= 360);
+  var newAngle = currentAngle + (ANGLE_STEP * elapsed) / 1000.0;
+  currentAngle = (newAngle %= 360);
+
+   cAngle2 = cAngle2 + (cAngle2Step/1000.0) * elapsed; // advance;
+  if( cAngle2 < 20.0 ) { // did we go past lower limit?
+    cAngle2 = 20.0;                 // yes. Stop at limit, and
+    if(cAngle2Step < 0) cAngle2Step = -cAngle2Step; // go FORWARDS
+    }
+  if( cAngle2 > 200.0 ) { // did we go past upper limit?
+   cAngle2 = 200.0;                 // yes. Stop at limit, and
+   if(cAngle2Step > 0) cAngle2Step = -cAngle2Step; // go BACKWARDS
+  }
 }
 
-function initVertexBuffers(gl) {
-  var t = (1 + Math.sqrt(5)) / 2;
-  var r = 1 / t;
+
+function makeHeart(){
+  var data = [];
+
+  //Formula from http://mathworld.wolfram.com/HeartCurve.html
+  for (let i = 0; i < 360; i++) {
+    t = i;
+    x = 16 * Math.pow(Math.sin(t),3);
+    y = 13 * Math.cos(t) - 5* Math.cos(2*t) - 2 * Math.cos(3*t) - Math.cos(4*t)
+    data = data.concat([x/10,y/10,0.0,1.0]);
+  }
+
+  var colors = [];
+
+  for(let j=0;j<360;j++){
+      colors.push(1.0);
+      colors.push(0.4);
+      colors.push(0.4);
+  }
+
+  appendPositions(data);
+  
+  appendColors(colors);
+
+}
+
+function makeMess(){
+   var t = (1 + Math.sqrt(5)) / 2;
+    var r = 1 / t;
   //20 vertices
-  var vertices = new Float32Array([
+  var verticesPos = [
     // (±1, ±1, ±1)
-    -1, -1, -1, -1, -1, 1, -1, 1, -1, -1, 1, 1,
-     1, -1, -1,  1, -1, 1,  1, 1, -1,  1, 1, 1,
+    -1, -1, -1, 1,
+     -1, -1, 1, 1,  
+     -1, 1, -1,1,  
+     -1, 1, 1,1,
+     1, -1, -1, 1,  
+      1, -1, 1, 1,   
+      1, 1, -1,1,  
+      1, 1, 1,1,
 
     // (0, ±1/φ, ±φ)
-     0, -r, -t,  0, -r, t,  0, r, -t,  0, r, t,
+     0, -r, -t,1, 
+      0, -r, t,1, 
+       0, r, -t,1,  
+       0, r, t,1,
 
     // (±1/φ, ±φ, 0) 
-    -r, -t,  0, -r,  t, 0,  r, -t, 0,  r, t, 0,
+    -r, -t,  0,1,  
+    -r,  t, 0,1,  
+    r, -t, 0,1,   
+    r, t, 0,1,
 
     // (±φ, 0, ±1/φ)
-    -t,  0, -r,  t,  0, -r, -t, 0, r,  t, 0, r
-  ]);
+    -t,  0, -r,1,  
+    t,  0, -r,1, 
+    -t, 0, r,1,  
+    t, 0, r,1,
+  ];
 
-  // Colors
-  var colors = new Float32Array([
+  var colors = [
     1.0, 0.4, 0.4, 1.0, 0.4, 0.4, 0.4, 0.4, 1.0, 0.4, 0.4, 1.0,
     1.0, 1.0, 1.0, 0.4, 1.0, 1.0, 0.4, 1.0, 0.4, 0.4, 1.0, 0.4, 
     1.0, 1.0, 1.0, 0.4, 1.0, 1.0, 1.0, 1.0, 1.0, 0.4, 1.0, 1.0, 
     1.0, 0.4, 0.4, 0.4, 0.4, 1.0, 0.4, 1.0, 1.0, 0.4, 1.0, 0.4, 
     1.0, 0.4, 0.4, 0.4, 1.0, 0.4, 1.0, 0.4, 0.4, 0.4, 1.0, 0.4 
-  ]);
+  ];
 
-  // Normal
-  var normals = new Float32Array([
-    -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0,
-    -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0,
-    -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 
-    -1.0, 0.0, 0.0, 0.0, 1.0, 0.0, -1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 
-    -1.0, 0.0, 0.0, 1.0, 0.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0, 0.0 
-  ]);
-  // Indices of the vertices
-  var indices = new Uint8Array([
-   3, 11, 7,  3, 7, 15,   3, 15, 13,
-    7, 19, 17,  7, 17, 6,   7, 6, 15,
-    17, 4, 8,   17, 8, 10,  17, 10, 6,
-    8, 0, 16,   8, 16, 2,   8, 2, 10,
-    0, 12, 1,   0, 1, 18,   0, 18, 16,
-    6, 10, 2,   6, 2, 13,   6, 13, 15,
-    2, 16, 18,  2, 18, 3,   2, 3, 13,
-    18, 1, 9,   18, 9, 11,  18, 11, 3,
-    4, 14, 12,  4, 12, 0,   4, 0, 8,
-    11, 9, 5,   11, 5, 19,  11, 19, 7,
-    19, 5, 14,  19, 14, 4,  19, 4, 17,
-    1, 12, 14,  1, 14, 5,   1, 5, 9
-  ]);
+   appendPositions(verticesPos);
 
-  // Write the vertex property to buffers (coordinates, colors and normals)
-  if (!initArrayBuffer(gl, "a_Position", vertices, 3, gl.FLOAT)) return -1;
-  if (!initArrayBuffer(gl, "a_Color", colors, 3, gl.FLOAT)) return -1;
-  if (!initArrayBuffer(gl, "a_Normal", normals, 3, gl.FLOAT)) return -1;
-
-  // Unbind the buffer object
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-  // Write the indices to the buffer object
-  var indexBuffer = gl.createBuffer();
-  if (!indexBuffer) {
-    console.log("Failed to create the buffer object");
-    return false;
-  }
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-
-  return indices.length;
-}
-
-function initArrayBuffer(gl, attribute, data, num, type) {
-  // Create a buffer object
-  var buffer = gl.createBuffer();
-  if (!buffer) {
-    console.log("Failed to create the buffer object");
-    return false;
-  }
-  // Write date into the buffer object
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
-  // Assign the buffer object to the attribute variable
-  var a_attribute = gl.getAttribLocation(gl.program, attribute);
-  if (a_attribute < 0) {
-    console.log("Failed to get the storage location of " + attribute);
-    return false;
-  }
-  gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
-  // Enable the assignment of the buffer object to the attribute variable
-  gl.enableVertexAttribArray(a_attribute);
-
-  return true;
+   appendColors(colors);
 }
